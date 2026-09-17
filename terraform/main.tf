43d811f9723148e7b36e20a6debc467a6a6ad36d
Dhananjay Dewangan <ddewangan1806@gmail.com>
2026-09-17 20:25:24 +0000

    feat: add postgres service

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,145 @@ resource "kubernetes_namespace_v1" "task_management" {
     name = "task-management"
   }
 }
+
+resource "kubernetes_secret_v1" "postgres" {
+  metadata {
+    name      = "postgres-secret"
+    namespace = kubernetes_namespace_v1.task_management.metadata[0].name
+  }
+
+  type = "Opaque"
+
+  data = {
+    POSTGRES_DB       = var.db_name
+    POSTGRES_USER     = var.db_user
+    POSTGRES_PASSWORD = var.db_password
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "postgres" {
+  metadata {
+    name      = "postgres-pvc"
+    namespace = kubernetes_namespace_v1.task_management.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+
+    storage_class_name = "standard"
+  }
+
+  wait_until_bound = false
+}
+
+resource "kubernetes_deployment_v1" "postgres" {
+  metadata {
+    name      = "postgres"
+    namespace = kubernetes_namespace_v1.task_management.metadata[0].name
+    labels = {
+      app = "postgres"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "postgres"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "postgres"
+        }
+      }
+
+      spec {
+        container {
+          name  = "postgres"
+          image = "postgres:17"
+
+          port {
+            container_port = 5432
+          }
+
+          env {
+            name = "POSTGRES_DB"
+
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret_v1.postgres.metadata[0].name
+                key  = "POSTGRES_DB"
+              }
+            }
+          }
+
+          env {
+            name = "POSTGRES_USER"
+
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret_v1.postgres.metadata[0].name
+                key  = "POSTGRES_USER"
+              }
+            }
+          }
+
+          env {
+            name = "POSTGRES_PASSWORD"
+
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret_v1.postgres.metadata[0].name
+                key  = "POSTGRES_PASSWORD"
+              }
+            }
+          }
+
+          volume_mount {
+            name       = "postgres-storage"
+            mount_path = "/var/lib/postgresql/data"
+          }
+        }
+
+        volume {
+          name = "postgres-storage"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim_v1.postgres.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "postgres" {
+  metadata {
+    name      = "postgres"
+    namespace = kubernetes_namespace_v1.task_management.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "postgres"
+    }
+
+    port {
+      port        = 5432
+      target_port = 5432
+      protocol    = "TCP"
+    }
+
+    type = "ClusterIP"
+  }
+}
