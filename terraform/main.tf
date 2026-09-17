541478d4f5f805a4a9f0473ed678f2764135e1cd
Dhananjay Dewangan <ddewangan1806@gmail.com>
2026-09-17 12:54:23 +0000

    feat: initialize terraform kubernetes provider

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
